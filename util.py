4b4c3c038fa0e91bae36d92ddb45ffafa1fe43be
Chih-Kai Yu <ChihKaiYu@Kais-MBP.local>
2016-04-01 18:02:59 +0800

    Rename bitPerSample to bytePerSample

--- util.py
+++ util.py
@@ -9,8 +9,8 @@ def enframe(y, frameSize, overlap):
 	step = frameSize - overlap
 	frameCount = int(np.floor((len(y)-overlap)/step))
 	out = np.matrix(np.zeros((frameSize, frameCount), dtype=np.float64))
-	for i in range(frameCount):
-		startIndex = i*step+1
+	for i in range(0, frameCount):
+		startIndex = i*step
 		out[:, i] = y[startIndex:(startIndex+frameSize), 0]
 	return out
 
@@ -18,18 +18,18 @@ def audioread(fileName):
 	#fs, au = scipy.io.wavfile.read(fileName)
 	audioFile = wave.open(fileName, 'rb')
 	# get file information and read sample data as string bytes
-	nChannels, bitPerSample, fs, nFrames = audioFile.getparams()[:4]
+	nChannels, bytePerSample, fs, nFrames = audioFile.getparams()[:4]
 	strData = audioFile.readframes(nFrames)
 	audioFile.close()
-	if bitPerSample > 4:
+	if bytePerSample > 4:
 		raise ValueError('Bit per sample can not be greater than 4.')
-	elif bitPerSample == 4:
-		au = np.matrix(np.fromstring(strData, dtype=np.int32)/(2**(bitPerSample*8-1)), dtype=np.float64).reshape(-1, nChannels)
-	elif bitPerSample == 3:
-		au = np.matrix(np.array([int.from_bytes(strData[i:i+bitPerSample], byteorder='little', signed=True) for i in range(0, len(strData), bitPerSample)], dtype=np.float64)/(2**(bitPerSample*8-1))).reshape(-1, nChannels)
-	elif bitPerSample == 2:
-		au = np.matrix(np.fromstring(strData, dtype=np.int16)/(2**(bitPerSample*8-1)), dtype=np.float64).reshape(-1, nChannels)
-	elif bitPerSample == 1:
+	elif bytePerSample == 4:
+		au = np.matrix(np.fromstring(strData, dtype=np.int32)/(2**(bytePerSample*8-1)), dtype=np.float64).reshape(-1, nChannels)
+	elif bytePerSample == 3:
+		au = np.matrix(np.array([int.from_bytes(strData[i:i+bytePerSample], byteorder='little', signed=True) for i in range(0, len(strData), bitPerSample)], dtype=np.float64)/(2**(bitPerSample*8-1))).reshape(-1, nChannels)
+	elif bytePerSample == 2:
+		au = np.matrix(np.fromstring(strData, dtype=np.int16)/(2**(bytePerSample*8-1)), dtype=np.float64).reshape(-1, nChannels)
+	elif bytePerSample == 1:
 		au = np.matrix((np.array(np.fromstring(strData, dtype=np.uint8), dtype=np.float64)-128)/(2**(bitPerSample*8-1))).reshape(-1, nChannels)
 	return (fs, au)
 
